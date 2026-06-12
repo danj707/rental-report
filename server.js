1fff7a6fad4c5745e0896e22e3992660f6402ab9
Daniel Jenner <danjenner@gmail.com>
2026-06-12 17:12:11 -0400

    Update apex/facility Metabase link -> c641a437-49c7-49f8-82bd-3417a7e3754b

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ const ORGS = {
     token:   "pcj5Qf0Wts7Wzc7P",
     orgId:   "aeba47d0-c97f-49cb-a0e9-93c5af3a68fa",
     logoUrl: "https://www.rec.us/_next/image?url=https%3A%2F%2Fprod-rec-tech-img-bucket-8656aa2.s3.us-west-1.amazonaws.com%2Forganization-aeba47d0-c97f-49cb-a0e9-93c5af3a68fa%2FfullLogo.png%3F1765923560125&w=1920&q=75",
-    facility: { mbUuid: "c876b1d7-df79-48c5-abf5-62917dee3534", defaultDateRange: 8, defaultLocationFilter: "Apex Center" },
+    facility: { mbUuid: "c641a437-49c7-49f8-82bd-3417a7e3754b", defaultDateRange: 8, defaultLocationFilter: "Apex Center" },
     programs: { mbUuid: "dee5b922-303f-47d9-abe3-75597410ad67" },
     "court-utilization": { mbUuid: "82d14a94-78ad-48d6-9531-11e72f53e285" },
     calendar: { mbUuid: "8a3dac9b-6c34-45e1-a7d0-3a177477fe17" },
