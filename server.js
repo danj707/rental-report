7ddc6848fa22d6d8bb137519222842f2dbccb81b
Daniel Jenner <danjenner@gmail.com>
2026-07-28 06:12:23 -0600

    Migrate dynamic orgs to server.js (town-of-clarkstown)

--- server.js
+++ server.js
@@ -726,6 +726,12 @@ const ORGS = {
     logoUrl: "https://www.rec.us/_next/image?url=https%3A%2F%2Fprod-rec-tech-img-bucket-8656aa2.s3.us-west-1.amazonaws.com%2Forganization-992ee322-4927-4558-827d-7f8768580b85%2FfullLogo.jpeg%3F1776960415666&w=2048&q=75",
     displayName: "Littleton PRCE",
   },
+  "town-of-clarkstown": {
+    token:   "Ac2HSGmYm3yb2eVP",
+    orgId:   "5dee565a-6012-4b4f-a325-0aea81674364",
+    logoUrl: "https://www.rec.us/_next/image?url=https%3A%2F%2Fprod-rec-tech-img-bucket-8656aa2.s3.us-west-1.amazonaws.com%2Forganization-5dee565a-6012-4b4f-a325-0aea81674364%2FfullLogo.png%3F1769719595995&w=2048&q=75",
+    displayName: "Town of Clarkstown",
+  },
 };
 
 const REPORT_TYPES = ["facility", "gl", "historic", "programs", "roster", "products", "memberships", "court-utilization", "calendar", "fasttrack", "users", "program-demographics", "instructor-payout", "retention", "annual-report", "section-detail", "ice-calendar", "qoq", "checkins", "program-checkins"];
