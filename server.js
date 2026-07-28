ae3ef13cb9cf4681a4bcd46037f5e1ff22744c42
Daniel Jenner <danjenner@gmail.com>
2026-07-28 14:07:15 -0600

    Migrate dynamic orgs to server.js (pawnee)

--- server.js
+++ server.js
@@ -738,6 +738,12 @@ const ORGS = {
     logoUrl: "https://www.rec.us/_next/image?url=https%3A%2F%2Fprod-rec-tech-img-bucket-8656aa2.s3.us-west-1.amazonaws.com%2Forganization-ff965a2b-7746-4de9-8b41-402927cb5879%2FfullLogo.png%3F1776783669314&w=2048&q=75",
     displayName: "Lewisburg",
   },
+  pawnee: {
+    token:   "dP6RfQYiWGAnQKJC",
+    orgId:   "9cc33597-701d-440f-a12f-f73643b7d229",
+    logoUrl: "https://www.rec.us/_next/image?url=https%3A%2F%2Fprod-rec-tech-img-bucket-8656aa2.s3.us-west-1.amazonaws.com%2Forganization-9cc33597-701d-440f-a12f-f73643b7d229%2FfullLogo.png%3F1741134162485&w=2048&q=75",
+    displayName: "City of Pawnee",
+  },
 };
 
 const REPORT_TYPES = ["facility", "gl", "historic", "programs", "roster", "products", "memberships", "court-utilization", "calendar", "fasttrack", "users", "program-demographics", "instructor-payout", "retention", "annual-report", "section-detail", "ice-calendar", "qoq", "checkins", "program-checkins"];
